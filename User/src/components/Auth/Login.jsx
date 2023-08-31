97abb27c361c8a1d42cce066a041f73f2a43a9a1
Parth_Ghatge <parthghatge982@gmail.com>
2023-08-31 22:34:12 +0530

    Added Missing Files.

---
+++ User/src/components/Auth/Login.jsx
@@ -0,0 +1,7 @@
+import AuthForm from "./AuthForm";
+
+const Login = () => {
+  return <AuthForm type="login" />;
+};
+
+export default Login;
---
+++ User/src/components/Auth/Signup.jsx
@@ -0,0 +1,7 @@
+import AuthForm from "./AuthForm";
+
+const SignUp = () => {
+  return <AuthForm type="signup" />;
+};
+
+export default SignUp;
